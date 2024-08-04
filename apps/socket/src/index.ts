b633770877ab4bc49804f74b840b6249d5cb64ed
Dhruvil Mehta <dhruvilmehta2606@gmail.com>
2024-08-04 11:59:25 -0700

    Added socket and http health check on same port

--- apps/socket/src/index.ts
+++ apps/socket/src/index.ts
@@ -2,6 +2,7 @@ import { UserManager } from "./UserManager";
 import { WebSocketServer } from 'ws';
 import dotenv from 'dotenv'
 import express from 'express'
+import http from 'http'
 
 dotenv.config()
 
@@ -11,12 +12,14 @@ app.get("/", (req, res) => {
     return res.status(200).send("Socket Working")
 })
 
-const wss = new WebSocketServer({ port: 3002 });
+const server = http.createServer(app);
+const wss = new WebSocketServer({ server });
+// const wss = new WebSocketServer({ port: 3002 });
 
 wss.on("connection", (ws) => {
     UserManager.getInstance().addUser(ws);
 });
 
-app.listen(3000, () => {
+server.listen(3000, () => {
     console.log("Socket running http on port 3000")
 })
